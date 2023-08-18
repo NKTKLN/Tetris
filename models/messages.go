0ee797a4b74e6bf985d1c2ed290db1a1eee6c324
NKTKLN <nktkln@nktkln.com>
2023-08-18 15:10:01 +0300

    Minor edits

--- models/messages.go
+++ models/messages.go
@@ -3,10 +3,6 @@ package models
 // Version data
 const Version string = "1.0.0"
 
-// Error messages
-var SizeErrorMessage string = "\033[1;31mTerminal window size is smaller than 20x17!\033[0m"
-var StatisticalMessage string = "\033[1;37mScore: \033[1;35m%05d \033[1;37m\nTime:  \033[1;36m%s\033[0m\n"
-
 // Informational messages
 var InfoMessage string = "" +
 	"Name:     \033[1;35mTetris\033[0m\n" +
@@ -22,3 +18,6 @@ var KeysMessage string = "" +
 	"\033[1;37m\"f\"\033[0m or \033[1;37mEnter\033[0m       - Moves the figure to the bottom.\n" +
 	"\033[1;37m\"r\"\033[0m or \033[1;37mArrow Up\033[0m    - Rotates the figure.\n" +
 	"\033[1;37m\"q\"\033[0m or \033[1;37mESC\033[0m         - Quits the game.\n"
+
+var SizeErrorMessage string = "\033[1;31mTerminal window size is smaller than 20x17!\033[0m"
+var StatisticalMessage string = "\033[1;37mScore: \033[1;35m%05d \033[1;37m\nTime:  \033[1;36m%s\033[0m\n"
